aae82c3018b5f5c53380d5b996751b75b59caa49
Arthur <96789842+ArthurReshetniak@users.noreply.github.com>
2022-01-13 18:23:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const mess = prompt("Ведите Имя");
+
+console.log(mess + " Ваше Имя");
+
+const nameOne = prompt("Ведите Ваш Возрост");
+
+console.log(nameOne + " Ваш Возрост");
+
+const nameTwo = prompt("Ведите Возрост Вашего Друга");
+
+console.log(nameTwo + " Возрост Вашего Друга");
+
+const a = 12;
+const b = 10;
+
+console.log(a + " A");
+console.log(b + " B");
+
+let a1 = 5+3;
+let a2 = 5-3;
+let a3 = 5*3;
+let a4 = 5/3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+let a5 = 5%3;
+let a6 = 3%5;
+let a7 = 5+3;
+let a8 = 5-3;
+
+alert(a5);
+alert(a6);
+alert(a7);
+alert(a8);
+
+
+
+
